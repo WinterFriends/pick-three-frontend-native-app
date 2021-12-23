99c4987b2ebb039a545c802d24a76ed2455cab43
ParkYucheon <yucheon6000@gmail.com>
2021-12-24 01:57:05 +0900

    Add class - Goal

---
+++ src/js/common/Goal.js
@@ -0,0 +1,50 @@
+class Goal {
+    /**
+     * 
+     * @param {number} id 
+     * @param {string} name 
+     * @param {string} description 
+     * @param {string} icon 
+     */
+    constructor(id, name, description, icon) {
+        this._id = id;
+        this._name = name;
+        this._description = description;
+        this._icon = icon;
+    }
+
+    getName() {
+        return this._name;
+    }
+
+    getDescription() {
+        return this._description;
+    }
+
+    getIcon() {
+        return this._icon;
+    }
+
+    toJson() {
+        let json = {
+            id: this._id,
+            name: this._name,
+            description: this._description,
+            icon: this._icon,
+        };
+
+        return json;
+    }
+
+    static fromJson(json) {
+        let goal = new Goal(json.id, json.name, json.description, json.icon);
+        return goal;
+    }
+
+    clone() {
+        let clone = new Goal(this._id, this._name, this._description, this._icon);
+        return clone;
+    }
+}
+
+export default Goal;
