57d47771918273eec21bd8232cd6d8a15a17632d
ParkYucheon <yucheon6000@gmail.com>
2021-12-27 08:25:31 +0900

    Update 'WriteDiaryScreen'

---
+++ src/js/components/UserGoalDiaryElement.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, TextInput } from "react-native";
+
+class UserGoalDiaryElement extends React.Component {
+
+    _maxCount = 200;
+
+    constructor(props) {
+        console.log("UserGoalDiaryElement.constructor: ", props);
+        super(props);
+        this.userGoal = props.userGoal;
+        this.goal = this.userGoal.getGoal();
+        this.state = {
+            text: (!this.userGoal.getDiary() || this.userGoal.getDiary().length === 0) ? "" : this.userGoal.getDiary()
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.title}>
+                    <Text>{this.goal.getIcon()}</Text>
+                    <Text>{this.goal.getName()}</Text>
+                </View>
+                <TextInput
+                    style={styles.diaryInput}
+                    multiline
+                    placeholder="일기를 입력해주세요."
+                    maxLength={this._maxCount}
+                    onChangeText={text => this.setState({ text })}
+                    value={this.state.text} />
+                <Text>{this.state.text.length}/{this._maxCount}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        backgroundColor: "green"
+    },
+    title: {
+        flexDirection: "row",
+        marginBottom: 10
+    },
+    diaryInput: {
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+        borderRadius: 20,
+        backgroundColor: "tomato"
+    }
+});
+
+export default UserGoalDiaryElement;
--- src/js/screens/WriteDiaryScreen.js
+++ src/js/screens/WriteDiaryScreen.js
@@ -1,11 +1,90 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, ScrollView, TouchableOpacity, StyleSheet, Alert } from "react-native";
+import Goal from "../common/Goal";
+import UserGoal from "../common/UserGoal";
+import UserGoalDiaryElement from "../components/UserGoalDiaryElement";
+
+let userGoalList = [
+    new UserGoal(
+        '2021-12-22',
+        new Goal(1, "일", "일 열심히!", "일 아이콘"),
+        false,
+        "열심히 일을 했다..?"
+    ),
+    new UserGoal(
+        '2021-12-22',
+        new Goal(2, "건강", "건강 열심히!", "건강 아이콘"),
+        false,
+        "열심히 건강을 했다..?"
+    ),
+    new UserGoal(
+        '2021-12-22',
+        new Goal(3, "가족", "가족 열심히!", "가족 아이콘"),
+        false,
+        "열심히 가족을 했다..?"
+    ),
+]
 
 class WriteDiaryScreen extends React.Component {
+
+    userGoalIndex = 0;
+
+    constructor(props) {
+        super(props);
+        this.userGoalList = [...userGoalList];
+    }
+
+    onPressBackButton() {
+        this.props.navigation.goBack(null);
+    }
+
+    onPressDeleteAllButton() {
+        console.log("WriteDiaryScreen.onPressDeleteAllButton");
+        let navigation = this.props.navigation;
+        Alert.alert(
+            "일기 삭제",
+            "일기를 삭제하시겠습니까?",
+            [
+                {
+                    text: "취소",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                {
+                    text: "확인", onPress: () => navigation.goBack(null)
+                }
+            ]
+        )
+    }
+
+    onPressConfirm() {
+        this.props.navigation.goBack(null);
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>WriteDiaryScreen</Text>
+                <View style={styles.buttons}>
+                    <Button title="back" onPress={this.onPressBackButton.bind(this)} />
+                    <Button title="delete all" onPress={this.onPressDeleteAllButton.bind(this)} />
+                </View>
+
+                <View style={styles.title}>
+                    <Text>Today's Diary</Text>
+                    <Text>오늘의 일기를 작성해주세요.</Text>
+                </View>
+
+                <ScrollView style={styles.scrollView} contentContainerStyle={{ padding: 20 }}>
+                    {
+                        this.userGoalList.map(userGoal => {
+                            return <UserGoalDiaryElement userGoal={userGoal} key={this.userGoalIndex++} />;
+                        })
+                    }
+                </ScrollView>
+
+                <TouchableOpacity style={styles.confirmButton} onPress={this.onPressConfirm.bind(this)}>
+                    <Text>저장</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -14,9 +93,28 @@ class WriteDiaryScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        paddingTop: 20
     },
+    buttons: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 20,
+        marginBottom: 20,
+        paddingHorizontal: 30
+    },
+    title: {
+        alignItems: "center"
+    },
+    scrollView: {
+        width: "100%",
+        backgroundColor: "teal"
+    },
+    confirmButton: {
+        width: "100%",
+        paddingVertical: 20,
+        alignItems: "center",
+        backgroundColor: "yellow"
+    }
 });
 
 export default WriteDiaryScreen;
