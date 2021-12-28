db712dd73cbde5d9c48745f7beceff59da8b0527
ParkYucheon <yucheon6000@gmail.com>
2021-12-29 07:20:47 +0900

    Add button event for 'EditProfileScreen'

--- src/js/screens/EditProfileScreen.js
+++ src/js/screens/EditProfileScreen.js
@@ -7,10 +7,32 @@ class EditProfileScreen extends React.Component {
         super(props);
     }
 
+    goBack() {
+        let goBackAble = this.props.route.params && this.props.route.params.goBackAble;
+
+        console.log(`EditProfileScreen.goBack: goBackAble = ${goBackAble}`);
+
+        if (goBackAble)
+            this.props.navigation.goBack(null);
+        else
+            this.props.navigation.navigate("TabNavigationScreen");
+    }
+
+    onPressCancelButton() {
+        console.log("EditProfileScreen.onPressCancelButton");
+        this.goBack();
+    }
+
+    onPressSaveProfileButton() {
+        console.log("EditProfileScreen.onPressSaveProfileButton");
+        // Save
+        this.goBack();
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <TouchableOpacity onPress={() => { this.props.navigation.navigate("TabNavigationScreen") }}>
+                <TouchableOpacity onPress={this.onPressCancelButton.bind(this)}>
                     <Text>건너뛰기</Text>
                 </TouchableOpacity>
 
@@ -24,7 +46,7 @@ class EditProfileScreen extends React.Component {
                 <Text>생년월일</Text>
                 <TextInput placeholder="생년월일을 입력해주세요" />
 
-                <Button title="픽플 시작하기" />
+                <Button title="픽플 시작하기" onPress={this.onPressSaveProfileButton.bind(this)} />
 
                 <StatusBar />
             </View>
