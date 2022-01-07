a7fa543c932a10c72928e73e47102b7bb3a7a206
yucheon6000 <yucheon6000@gmail.com>
2022-01-07 19:22:29 +0900

    Update 'GoalManager' class (remove test code) (add 'isReady' public method)

--- src/js/managers/GoalManager.js
+++ src/js/managers/GoalManager.js
@@ -3,6 +3,7 @@ import Goal from "../common/Goal";
 class GoalManager {
     static _goalList = [];
     static _goalById = {};
+    static _isReady = false;
 
     static setGoalList(goalList) {
         let freezeGoalList = [];
@@ -16,6 +17,9 @@ class GoalManager {
         freezeGoalById = Object.freeze(freezeGoalById);
 
         this._goalById = freezeGoalById;
+
+        // ready
+        this._isReady = true;
     }
 
     static getGoalList() {
@@ -25,14 +29,10 @@ class GoalManager {
     static getGoalById(id) {
         return this._goalById[id];
     }
-}
 
-GoalManager.setGoalList([
-    new Goal(0, "일", "시간을 투자한 대가로 가치를 얻는 모든 활동", "일 아이콘"),
-    new Goal(1, "수면", "하루의 30%를 차지하는 중요한 시간", "수면 아이콘"),
-    new Goal(2, "가족", "태어난 가족, 선택한 가족이든 자신이 정의한 가족", "가족 아이콘"),
-    new Goal(3, "건강", "신체적, 정신적 건강을 모두 포함하는 자기 관리", "건강 아이콘"),
-    new Goal(4, "친구", "일, 가족 이외에 즐거움을 주는 모든 사람이나 활동", "친구 아이콘")
-])
+    static isReady() {
+        return this._isReady;
+    }
+}
 
 export default GoalManager;
