fb43720c65b4ca3f5ef27a1afbfeeaf557b3620b
yucheon6000 <yucheon6000@gmail.com>
2022-01-07 19:10:57 +0900

    Update 'Goal' class (remove icon member variable) (add activeIcon, inactiveIcon, mainColor, subColor)

--- src/js/common/Goal.js
+++ src/js/common/Goal.js
@@ -1,16 +1,21 @@
 class Goal {
     /**
-     * 
      * @param {number} id 
      * @param {string} name 
      * @param {string} description 
-     * @param {string} icon 
+     * @param {string} activeIcon 
+     * @param {string} inactiveIcon 
+     * @param {string} mainColor 
+     * @param {string} subColor
      */
-    constructor(id, name, description, icon) {
+    constructor(id, name, description, activeIcon, inactiveIcon, mainColor, subColor) {
         this._id = id;
         this._name = name;
         this._description = description;
-        this._icon = icon;
+        this._activeIcon = activeIcon;
+        this._inactiveIcon = inactiveIcon;
+        this._mainColor = mainColor;
+        this._subColor = subColor;
     }
 
     getId() {
@@ -26,27 +31,56 @@ class Goal {
     }
 
     getIcon() {
-        return this._icon;
+        return this.getActiveIcon();
+    }
+
+    getActiveIcon() {
+        return this._activeIcon;
+    }
+
+    getInactiveIcon() {
+        return this._inactiveIcon;
+    }
+
+    getMainColor() {
+        return this._mainColor;
+    }
+
+    getSubColor() {
+        return this._subColor;
     }
 
     toJson() {
         let json = {
-            id: this._id,
-            name: this._name,
-            description: this._description,
-            icon: this._icon,
+            id: this.getId(),
+            name: this.getName(),
+            description: this.getDescription(),
+            activeIcon: this.getActiveIcon(),
+            inactiveIcon: this.getInactiveIcon(),
+            mainColor: this.getMainColor(),
+            subColor: this.getSubColor()
         };
 
         return json;
     }
 
     static fromJson(json) {
-        let goal = new Goal(json.id, json.name, json.description, json.icon);
+        let goal = new Goal(
+            json.id, json.name,
+            json.description,
+            json.activeIcon, json.inactiveIcon,
+            json.mainColor, json.subColor
+        );
         return goal;
     }
 
     clone() {
-        let clone = new Goal(this._id, this._name, this._description, this._icon);
+        let clone = new Goal(
+            this.getId(), this.getName(),
+            this.getDescription(),
+            this.getActiveIcon(), this.getInactiveIcon(),
+            this.getMainColor(), this.getSubColor()
+        );
         return clone;
     }
 }
