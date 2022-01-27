8f4b9985a7cfc4483174c6a5735471d0fa7b2e1d
yucheon6000 <yucheon6000@gmail.com>
2022-01-28 02:07:20 +0900

    Update 'Color' class

--- src/js/common/Colors.js
+++ src/js/common/Colors.js
@@ -2,9 +2,23 @@
 
 class Colors {
     static white = "#FFFFFF";
+
+    static black01 = "#000000";
+    static black02 = "#3D3D3D";
+    static black03 = "#494949";
+    static black04 = "#A0A0A0";
+    static black05 = "#BABABA";
+    static black06 = "#747474";
+
+    static gray01 = "#EFEFEF";
+    static gray02 = "#B0B0B0";
+    static gray03 = "#929292";
+
     static primary01 = "#FFD155";
     static primary02 = "#FFE399";
     static primary03 = "#FFF2CC";
+
+    static background = "#FCFBF9"
 }
 
 export default Colors;
