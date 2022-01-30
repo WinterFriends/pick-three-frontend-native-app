658b65d7d6a119c0895bd94f6f197e2a93a02f92
yucheon6000 <yucheon6000@gmail.com>
2022-01-30 15:11:33 +0900

    Update 'GoalManager' class (add 'getGoalIdList' method and update 'setGoalList' method)

--- src/js/managers/GoalManager.js
+++ src/js/managers/GoalManager.js
@@ -2,21 +2,28 @@ import Goal from "../common/Goal";
 
 class GoalManager {
     static _goalList = [];
+    static _goalIdList = [];
     static _goalById = {};
     static _isReady = false;
 
     static setGoalList(goalList) {
-        let freezeGoalList = [];
-        goalList.map(goal => freezeGoalList.push(Object.freeze(goal)));
-        freezeGoalList = Object.freeze(freezeGoalList);
+        this._goalList = [];
+        this._goalIdList = [];
+        this._goalById = {};
 
-        this._goalList = freezeGoalList;
+        goalList.map(goal => {
+            let goalId = goal.getId();
+            let freezedGoal = Object.freeze(goal);
+            this._goalList.push(freezedGoal);
+            this._goalIdList.push(goalId);
+            this._goalById[goalId] = freezedGoal;
+        });
 
-        let freezeGoalById = {};
-        freezeGoalList.map(goal => { freezeGoalById[goal.getId()] = goal });
-        freezeGoalById = Object.freeze(freezeGoalById);
+        this._goalList = Object.freeze(this._goalList);
+        this._goalIdList = Object.freeze(this._goalIdList.sort());
+        this._goalById = Object.freeze(this._goalById);
 
-        this._goalById = freezeGoalById;
+        console.log(this._goalById);
 
         // ready
         this._isReady = true;
@@ -28,6 +35,10 @@ class GoalManager {
 
     static getGoalById(id) {
         return this._goalById[id];
+    }
+
+    static getGoalIdList() {
+        return this._goalIdList;
     }
 
     static isReady() {
