54e565316ac4975c92fffbccca67f28e5572143b
yucheon6000 <yucheon6000@gmail.com>
2022-01-30 15:12:36 +0900

    Add week methods for 'DateUtils' class

--- src/js/utils/DateUtils.js
+++ src/js/utils/DateUtils.js
@@ -73,6 +73,103 @@ class DateUtils {
         let lastDate = new Date(year, month, 0);
         return lastDate.getDate();
     }
+
+
+    /**
+     * 해당하는 년, 월, 주를 알려주는 함수
+     * @param {Date} date 포맷팅된 날짜 문자열
+     * @returns 해당 일이 포함되는 년, 월, 주
+     */
+    static getWeekNumberByMonth(date) {
+        const inputDate = date;
+
+        // 인풋의 년, 월
+        let year = inputDate.getFullYear();
+        let month = inputDate.getMonth() + 1;
+
+        // 목요일 기준 주차 구하기
+        const weekNumberByThurFnc = (paramDate) => {
+
+            const year = paramDate.getFullYear();
+            const month = paramDate.getMonth();
+            const date = paramDate.getDate();
+
+            // 인풋한 달의 첫 날과 마지막 날의 요일
+            const firstDate = new Date(year, month, 1);
+            const lastDate = new Date(year, month + 1, 0);
+            const firstDayOfWeek = firstDate.getDay() === 0 ? 7 : firstDate.getDay();
+            const lastDayOfweek = lastDate.getDay();
+
+            // 인풋한 달의 마지막 일
+            const lastDay = lastDate.getDate();
+
+            // 첫 날의 요일이 금, 토, 일요일 이라면 true
+            const firstWeekCheck = firstDayOfWeek === 5 || firstDayOfWeek === 6 || firstDayOfWeek === 7;
+            // 마지막 날의 요일이 월, 화, 수라면 true
+            const lastWeekCheck = lastDayOfweek === 1 || lastDayOfweek === 2 || lastDayOfweek === 3;
+
+            // 해당 달이 총 몇주까지 있는지
+            const lastWeekNo = Math.ceil((firstDayOfWeek - 1 + lastDay) / 7);
+
+            // 날짜 기준으로 몇주차 인지
+            let weekNo = Math.ceil((firstDayOfWeek - 1 + date) / 7);
+
+            // 인풋한 날짜가 첫 주에 있고 첫 날이 월, 화, 수로 시작한다면 'prev'(전달 마지막 주)
+            if (weekNo === 1 && firstWeekCheck) weekNo = 'prev';
+            // 인풋한 날짜가 마지막 주에 있고 마지막 날이 월, 화, 수로 끝난다면 'next'(다음달 첫 주)
+            else if (weekNo === lastWeekNo && lastWeekCheck) weekNo = 'next';
+            // 인풋한 날짜의 첫 주는 아니지만 첫날이 월, 화 수로 시작하면 -1;
+            else if (firstWeekCheck) weekNo = weekNo - 1;
+
+            return weekNo;
+        };
+
+        // 목요일 기준의 주차
+        let weekNo = weekNumberByThurFnc(inputDate);
+
+        // 이전달의 마지막 주차일 떄
+        if (weekNo === 'prev') {
+            // 이전 달의 마지막날
+            const afterDate = new Date(year, month - 1, 0);
+            year = month === 1 ? year - 1 : year;
+            month = month === 1 ? 12 : month - 1;
+            weekNo = weekNumberByThurFnc(afterDate);
+        }
+        // 다음달의 첫 주차일 때
+        if (weekNo === 'next') {
+            year = month === 12 ? year + 1 : year;
+            month = month === 12 ? 1 : month + 1;
+            weekNo = 1;
+        }
+
+        return { year, month, weekNo };
+    }
+
+    /**
+     * 해당하는 주의 시작일을 알려주는 함수
+     * @param {Date} date
+     * @returns 해당주의 시작일 Date
+     */
+    static getFirstDayOfWeek(date) {
+        const offset = 86400000; // 하루의 밀리초
+
+        let prevDate = date;
+        let prevWeekNo = this.getWeekNumberByMonth(prevDate).weekNo;
+
+        while (true) {
+            let newDate = new Date(prevDate.getTime() - offset);
+            let newWeekNo = this.getWeekNumberByMonth(newDate).weekNo;
+
+            if (newWeekNo == prevWeekNo) {
+                prevDate = newDate;
+                prevWeekNo = newWeekNo;
+            }
+            else
+                break;
+        }
+
+        return prevDate;
+    }
 }
 
 export default DateUtils;
